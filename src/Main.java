8e4dbd65ee870da79727bdda693df7c24338f8e1
komat0 <111368527+komat0@users.noreply.github.com>
2023-03-19 18:29:59 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat marusya = new Cat();
+        Cat vasya = new Cat();
+        Cat barsik = new Cat();
+        Cat timokha = new Cat();
+        Cat agnus = new Cat();
+
+
+        System.out.println(marusya.getStatus());
+        System.out.println(marusya.getWeight());
+        System.out.println(barsik.getWeight());
+        System.out.println(timokha.getWeight());
+
+
+        while (vasya.getWeight() <= vasya.getMaxWeight()) {
+            vasya.feed(1000.0);
+            System.out.println("Васю покормили");
+        }
+        System.out.println("Вася " + vasya.getStatus());
+
+        System.out.println("Тимоха " + timokha.getStatus());
+
+        while (!Objects.equals(timokha.getStatus(), "Dead")) {
+            timokha.meow();
+        }
+        System.out.println("Тимоха " + timokha.getStatus());
+
+
+    }
+}
